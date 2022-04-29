054ce58ba2f4c687e911cdfa730003f0ed1d3d12
HaileyThomas <haileyraebeauty@gmail.com>
2022-04-29 10:43:03 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app-container">
+        <Header />
+        <div className="main-container">
+          <Pokelist />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
