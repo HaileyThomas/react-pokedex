798cedfbc91817fa7f3501fab617ea8327e9c4bc
HaileyThomas <haileyraebeauty@gmail.com>
2022-04-29 13:00:51 -0700

    complete basic layout

--- src/components/Pokelist/index.js
+++ src/components/Pokelist/index.js
@@ -3,7 +3,17 @@ import React from "react";
 function Pokelist() {
   return (
     <section className="pokelist-container">
-      <div className="search-container">search bar will go here</div>
+      <div className="search-container">
+        <label htmlFor="search-form">
+          <input
+            type="search"
+            name="search-form"
+            id="search-form"
+            className="search-input"
+            placeholder="Search pokemon by name"
+          />
+        </label>
+      </div>
       <div className="results-container">results will go here</div>
     </section>
   );
