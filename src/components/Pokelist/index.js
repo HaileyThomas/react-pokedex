43c69eaf7959e5b723b04c493494d563c9bfaee1
HaileyThomas <haileyraebeauty@gmail.com>
2022-04-29 14:01:29 -0700

    fetch and console log json data

--- src/components/Pokelist/index.js
+++ src/components/Pokelist/index.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Pokelist() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        data.results.forEach(function (pokemon) {
+          let url = pokemon.url;
+          fetch(url)
+            .then((response) => response.json())
+            .then(function (pokeData) {
+              console.log(pokeData);
+              setData(pokeData);
+            });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
+
+  const pokemon = data;
+
   return (
     <section className="pokelist-container">
       <div className="search-container">
