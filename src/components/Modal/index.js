a2d9679869adf7a68af77ee078fae2125f391600
HaileyThomas <haileyraebeauty@gmail.com>
2022-05-02 14:02:33 -0700

    set up div layout for pokedex

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -17,7 +17,26 @@ const Modal = (props) => {
             onClick={props.onClose}
           />
         </div>
-        <div className="modal-body-container"></div>
+        <div className="modal-body-container">
+          <div className="pokedex-container">
+            <div className="pokedex-top">
+              <div className="big-circle"></div>
+              <div className="three-circle-container"></div>
+            </div>
+            <div className="pokedex-screen">
+              <div className="screen-top"></div>
+              <div className="screen-main"></div>
+              <div className="screen-bottom"></div>
+            </div>
+            <div className="pokedex-buttons">
+              <div className="name-container">{props.pokemon.name}</div>
+              <div className="two-buttons-container"></div>
+              <div className="plus-sign-container">
+                <Icon icon="emojione-v1:heavy-plus-sign" className="plus-btn" />
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="modal-footer-container">
           <p className="modal-footer" onClick={props.onClose}>
             close pokedex
