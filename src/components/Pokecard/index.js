05795c4a6755e4cda44bfa3cbcf9dba74aeccbd9
HaileyThomas <haileyraebeauty@gmail.com>
2022-05-02 12:36:41 -0700

    finish pokecard design

--- src/components/Pokecard/index.js
+++ src/components/Pokecard/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Icon } from "@iconify/react";
 
 function Pokecard(pokemonData) {
   const [pokemon, setPokemon] = useState([]);
@@ -16,11 +17,27 @@ function Pokecard(pokemonData) {
   console.log(pokemon);
 
   return (
-    <section className="pokemon-card">
-      <h2>{pokemonData.name}</h2>
-      <br />
-      <p>{pokemonData.url}</p>
-      <br />
+    <section className="pokemon-card-container">
+      <div className="pokemon-card">
+        <div className="pokemon-card-header">
+          <p className="card-header">ID: {pokemon.id}</p>
+          <Icon icon="bx:expand-alt" className="card-expand-icon" />
+        </div>
+        <div className="poke-ball-container">
+          <div className="poke-ball">
+            <div className="poke-ball-image">
+              <img
+                src={pokemon.sprites.front_default}
+                alt={pokemon.name}
+                className="card-img"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="poke-card-name">
+          <h2 className="card-name">{pokemon.name}</h2>
+        </div>
+      </div>
     </section>
   );
 }
