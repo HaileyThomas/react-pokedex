5b583cdcd16e7df5c3eacc38cff37783865ff16d
HaileyThomas <haileyraebeauty@gmail.com>
2022-05-02 13:54:43 -0700

    set up basic modal structure

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
-function Modal() {
+const Modal = (props) => {
+  if (!props.show) {
+    return null;
+  }
+
   return (
-    <section className="modal-container">
-      <p>Modal will go here</p>
+    <section className="modal-container" onClick={props.onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header-container">
+          <h2 className="modal-header">{props.pokemon.name}</h2>
+          <Icon
+            icon="fa:close"
+            className="modal-close"
+            onClick={props.onClose}
+          />
+        </div>
+        <div className="modal-body-container"></div>
+        <div className="modal-footer-container">
+          <p className="modal-footer" onClick={props.onClose}>
+            close pokedex
+          </p>
+        </div>
+      </div>
     </section>
   );
-}
+};
 
 export default Modal;
