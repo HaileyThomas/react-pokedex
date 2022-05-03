cc53843a6c09111974a349daa10dd42e8730dac4
HaileyThomas <haileyraebeauty@gmail.com>
2022-05-03 12:47:37 -0700

    make sure up to date

--- src/components/Pokelist/index.js
+++ src/components/Pokelist/index.js
@@ -18,17 +18,6 @@ function Pokelist() {
 
   return (
     <section className="pokelist-container">
-      <div className="search-container">
-        <label htmlFor="search-form">
-          <input
-            type="search"
-            name="search-form"
-            id="search-form"
-            className="search-input"
-            placeholder="Search pokemon by name"
-          />
-        </label>
-      </div>
       <div className="results-container">
         {Object.entries(allPokemon)[3] &&
           Object.entries(allPokemon)[3][1].map((pokemonData, index) => {
